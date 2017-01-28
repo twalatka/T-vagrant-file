57d884b20555fda616b9a8782b4cfe2f9f153ac5
twalatka <twalatka@gmail.com>
2017-01-28 12:05:06 -0700

    Vagrant Files
    Adding Remaining files from Vagrand Folder

---
+++ ExampleScript.sh
@@ -0,0 +1,2 @@
+#1/bin/sh
+echo "Hello World"
---
+++ Examplenodescript.js
@@ -0,0 +1,15 @@
+var puzzlers = [
+    function(input) {
+        return (input * 3) - 8;
+    },
+    function(input) {
+        return (input + 2) * (input + 2) * (input + 2);
+    },
+    function(input) {
+        return (input * input) - 9;
+    },
+    function(input) {
+        return input % 4;
+    }
+
+];
